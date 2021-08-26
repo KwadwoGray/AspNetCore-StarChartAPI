978895ef54395ba4f6a4d0cd0e6e72d0bc7ed164
unknown <kwadwo.gray@gmail.com>
2021-08-26 16:57:46 -0500

    Making changes on Step 4.

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    
+    public class CelestialObjectController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        RouteAttribute routeAttribute;
+        ApiControllerAttribute apiControllerAttribute;
+
+
+    }
+}
